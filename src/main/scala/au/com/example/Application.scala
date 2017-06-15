de21000b14e4c08526457b5586d4222935f4a42b
Ashlin Eldridge <aeldridge@seek.com.au>
2017-06-15 14:55:23 +1000

    Added spark submit task

--- src/main/scala/au/com/example/Application.scala
+++ src/main/scala/au/com/example/Application.scala
@@ -1,14 +1,25 @@
 package au.com.example
 
 import com.typesafe.scalalogging.LazyLogging
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
+import org.apache.spark.{SparkConf, SparkContext}
 
 object Application extends App with LazyLogging {
-  val conf = new SparkConf().setMaster("local").setAppName("My App")
-  val sc = new SparkContext(conf)
-  val lines = sc.textFile("/usr/local/opt/apache-spark/README.md")
-  val pyLines = lines.filter(_.contains("Python"))
-  logger.info(s"Number of lines is: ${pyLines.count()}")
+
+  parseArgs match {
+    case (Some(inputFile), Some(outputFile)) =>
+      val conf = new SparkConf().setMaster("local").setAppName("WordCount")
+      val sc = new SparkContext(conf)
+      val lines = sc.textFile(inputFile)
+      val words = lines.flatMap(_.split("\\s"))
+      val counts = words.map(w => (w, 1)).reduceByKey { case (x, y) => x + y }
+      counts.saveAsTextFile(outputFile)
+      logger.info("Done")
+    case _ =>
+      logger.error("Error: Missing arguments")
+  }
+
+  def parseArgs: (Option[String], Option[String]) = args.toList match {
+    case h :: t => (Some(h), t.headOption)
+    case _      => (None, None)
+  }
 }
